74b84fab8c3e4cc17272bbd54276d63d74284e45
danprince <danielprince@hotmail.co.uk>
2015-11-28 16:11:50 +0530

    fixes typos

--- spec/watcher_spec.rb
+++ spec/watcher_spec.rb
@@ -12,7 +12,7 @@ describe(Jekyll::Watcher) do
 
   let(:options) { base_opts }
   let(:site)    { instance_double(Jekyll::Site) }
-  let(:default_ignored) { [/_config\.yml/, /#{Regex.escape('_site/')}/, /\.jekyll\-metadata/] }
+  let(:default_ignored) { [/_config\.yml/, /#{Regexp.escape('_site/')}/, /\.jekyll\-metadata/] }
   subject { described_class }
   before(:each) do
     FileUtils.mkdir(options["destination"]) if options["destination"]
@@ -125,7 +125,7 @@ describe(Jekyll::Watcher) do
     end
 
     context "with a custom destination" do
-      let(:default_ignored) { [/_config\.yml/, /#{Regex.escape('_dest/')}/, /\.jekyll\-metadata/] }
+      let(:default_ignored) { [/_config\.yml/, /#{Regexp.escape('_dest/')}/, /\.jekyll\-metadata/] }
 
       context "when source is absolute" do
         context "when destination is absolute" do
