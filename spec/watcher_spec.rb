86725d124426904f021e88a9e868c5da7e4b4ffc
danprince <danielprince@hotmail.co.uk>
2015-11-28 15:59:36 +0530

    apply @envygeeks patch

--- spec/watcher_spec.rb
+++ spec/watcher_spec.rb
@@ -10,7 +10,7 @@ describe(Jekyll::Watcher) do
 
   let(:options) { base_opts }
   let(:site)    { instance_double(Jekyll::Site) }
-  let(:default_ignored) { [/_config\.yml/, /_site\//, /\.jekyll\-metadata/] }
+  let(:default_ignored) { [/_config\.yml/, /#{Regex.escape('_site/')}/, /\.jekyll\-metadata/] }
   subject { described_class }
   before(:each) do
     FileUtils.mkdir(options['destination']) if options['destination']
@@ -88,7 +88,7 @@ describe(Jekyll::Watcher) do
     end
 
     context "with a custom destination" do
-      let(:default_ignored) { [/_config\.yml/, /_dest\//, /\.jekyll\-metadata/] }
+      let(:default_ignored) { [/_config\.yml/, /#{Regex.escape('_dest/')}/, /\.jekyll\-metadata/] }
 
       context "when source is absolute" do
         context "when destination is absolute" do
