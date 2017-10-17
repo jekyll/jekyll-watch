0610a3d1a6a663acc40b8ca7db6ffe395faaab67
Frank Taillandier <frank.taillandier@gmail.com>
2017-10-17 20:54:20 +0200

    Inherit Jekyll's rubocop config for consistency

--- lib/jekyll-watch.rb
+++ lib/jekyll-watch.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 require_relative "jekyll/watcher"
 require_relative "jekyll/commands/watch"
--- spec/watcher_spec.rb
+++ spec/watcher_spec.rb
@@ -1,32 +1,36 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require "spec_helper"
 
 describe(Jekyll::Watcher) do
   let(:base_opts) do
     {
-      'source' => source_dir,
-      'destination' => dest_dir
+      "source"      => source_dir,
+      "destination" => dest_dir,
     }
   end
 
   let(:options) { base_opts }
   let(:site)    { instance_double(Jekyll::Site) }
-  let(:default_ignored) { [/_config\.yml/, /_site/, /\.jekyll\-metadata/] }
+  let(:default_ignored) { [%r!_config\.yml!, %r!_site!, %r!\.jekyll\-metadata!] }
   subject { described_class }
   before(:each) do
-    FileUtils.mkdir(options['destination']) if options['destination']
+    FileUtils.mkdir(options["destination"]) if options["destination"]
   end
 
   after(:each) do
-    FileUtils.rm_rf(options['destination']) if options['destination']
+    FileUtils.rm_rf(options["destination"]) if options["destination"]
   end
 
   describe "#watch" do
     let(:listener) { instance_double(Listen::Listener) }
 
-    let(:opts) { { ignore: default_ignored, force_polling: options['force_polling'] } }
+    let(:opts) do
+      { :ignore => default_ignored, :force_polling => options["force_polling"] }
+    end
 
     before do
-      allow(Listen).to receive(:to).with(options['source'], opts).and_return(listener)
+      allow(Listen).to receive(:to).with(options["source"], opts).and_return(listener)
 
       allow(listener).to receive(:start)
 
@@ -38,27 +42,33 @@ describe(Jekyll::Watcher) do
       subject.watch(options)
     end
 
-    it 'starts the listener' do
+    it "starts the listener" do
       expect(listener).to have_received(:start)
     end
 
-    it 'sleeps' do
+    it "sleeps" do
       expect(subject).to have_received(:sleep_forever)
     end
 
     it "ignores the config and site by default" do
-      expect(Listen).to have_received(:to).with(anything, hash_including(ignore: default_ignored))
+      expect(Listen)
+        .to have_received(:to)
+        .with(anything, hash_including(:ignore => default_ignored))
     end
 
     it "defaults to no force_polling" do
-      expect(Listen).to have_received(:to).with(anything, hash_including(force_polling: nil))
+      expect(Listen)
+        .to have_received(:to)
+        .with(anything, hash_including(:force_polling => nil))
     end
 
     context "with force_polling turned on" do
-      let(:options)  { base_opts.merge('force_polling' => true) }
+      let(:options) { base_opts.merge("force_polling" => true) }
 
       it "respects the custom value of force_polling" do
-        expect(Listen).to have_received(:to).with(anything, hash_including(force_polling: true))
+        expect(Listen)
+          .to have_received(:to)
+          .with(anything, hash_including(:force_polling => true))
       end
     end
   end
@@ -66,10 +76,13 @@ describe(Jekyll::Watcher) do
   describe "#watch using site instance" do
     let(:listener) { instance_double(Listen::Listener) }
 
-    let(:opts) { { ignore: default_ignored, force_polling: nil } }
+    let(:opts) { { :ignore => default_ignored, :force_polling => nil } }
 
     before do
-      allow(Listen).to receive(:to).with(options['source'], opts).and_return(listener)
+      allow(Listen)
+        .to receive(:to)
+        .with(options["source"], opts)
+        .and_return(listener)
 
       allow(listener).to receive(:start)
 
@@ -80,14 +93,14 @@ describe(Jekyll::Watcher) do
       subject.watch(options, site)
     end
 
-    it 'does not create a new site instance' do
+    it "does not create a new site instance" do
       expect(listener).to have_received(:start)
     end
   end
 
   context "#listen_ignore_paths" do
     let(:ignored) { subject.listen_ignore_paths(options) }
-    let(:metadata_path) { Jekyll.sanitized_path(options['source'], '.jekyll-metadata') }
+    let(:metadata_path) { Jekyll.sanitized_path(options["source"], ".jekyll-metadata") }
 
     before(:each) { FileUtils.touch(metadata_path) }
     after(:each)  { FileUtils.rm(metadata_path) }
@@ -97,31 +110,33 @@ describe(Jekyll::Watcher) do
     end
 
     context "with something excluded" do
-      let(:excluded) { ['README.md', 'LICENSE'] }
-      let(:excluded_absolute) { excluded.map { |p| Jekyll.sanitized_path(options['source'], p) }}
-      let(:options) { base_opts.merge('exclude' => excluded) }
+      let(:excluded) { ["README.md", "LICENSE"] }
+      let(:excluded_absolute) do
+        excluded.map { |p| Jekyll.sanitized_path(options["source"], p) }
+      end
+      let(:options) { base_opts.merge("exclude" => excluded) }
       before(:each) { FileUtils.touch(excluded_absolute) }
       after(:each)  { FileUtils.rm(excluded_absolute) }
 
       it "ignores the excluded files" do
-        expect(ignored).to include(/README\.md/)
-        expect(ignored).to include(/LICENSE/)
+        expect(ignored).to include(%r!README\.md!)
+        expect(ignored).to include(%r!LICENSE!)
       end
     end
 
     context "with a custom destination" do
-      let(:default_ignored) { [/_config\.yml/, /_dest/, /\.jekyll\-metadata/] }
+      let(:default_ignored) { [%r!_config\.yml!, %r!_dest!, %r!\.jekyll\-metadata!] }
 
       context "when source is absolute" do
         context "when destination is absolute" do
-          let(:options) { base_opts.merge('destination' => source_dir('_dest')) }
+          let(:options) { base_opts.merge("destination" => source_dir("_dest")) }
           it "ignores the destination" do
             expect(ignored).to eql(default_ignored)
           end
         end
 
         context "when destination is relative" do
-          let(:options) { base_opts.merge('destination' => 'spec/test-site/_dest') }
+          let(:options) { base_opts.merge("destination" => "spec/test-site/_dest") }
           it "ignores the destination" do
             expect(ignored).to eql(default_ignored)
           end
@@ -129,24 +144,27 @@ describe(Jekyll::Watcher) do
       end
 
       context "when source is relative" do
-        let(:base_opts) { {'source' => Pathname.new(source_dir).relative_path_from(Pathname.new('.').expand_path).to_s } }
+        let(:base_opts) do
+          { "source" => Pathname
+            .new(source_dir)
+            .relative_path_from(Pathname.new(".")
+            .expand_path).to_s, }
+        end
 
         context "when destination is absolute" do
-          let(:options) { base_opts.merge('destination' => source_dir('_dest')) }
+          let(:options) { base_opts.merge("destination" => source_dir("_dest")) }
           it "ignores the destination" do
             expect(ignored).to eql(default_ignored)
           end
         end
 
         context "when destination is relative" do
-          let(:options) { base_opts.merge('destination' => 'spec/test-site/_dest') }
+          let(:options) { base_opts.merge("destination" => "spec/test-site/_dest") }
           it "ignores the destination" do
             expect(ignored).to eql(default_ignored)
           end
         end
       end
     end
-
   end
-
 end
